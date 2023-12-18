768ecc21c82543209ce6571e17adebcfe588c762
Mummy <amirseraj.ir@gmail.com>
2023-12-18 14:04:17 +0100

    Initilize Project with SC and vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Navbar } from "./sections";
+
+// import styled from "styled-components";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import StyledButton from "../../components/Button/Button";
+
+const Navbar = () => {
+  return (
+    <div>
+      <StyledButton>---</StyledButton>
+    </div>
+  );
+};
+
+export default Navbar;
